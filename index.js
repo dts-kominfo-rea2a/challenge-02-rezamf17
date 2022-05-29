113aaaad9c1b069ac72e81c65150ac01adb95eeb
reza.muhamad <reza.muhamad@pactindo.com>
2022-05-29 20:20:55 +0700

    challenge alur logika

--- index.js
+++ index.js
@@ -24,19 +24,31 @@ function lakukanLooping(arrPegawai) {
       Contoh: ["Aisyah Nirmala", "Mansur Faisal", ...]
   */
   let hasilLooping = null;
+  // console.log(dataYangAkanDilooping)
+  for (let i = 0; i < dataYangAkanDilooping.length; i++) {
+     const loop = dataYangAkanDilooping[i]
+      console.log(`${loop.namaDepan} ${loop.namaBelakang}`)
+  }
 
   /*
     TODO 2: Buatlah sebuah variabel bernama "jumlahPria"
       yang berisi jumlah pria dari masing masing pegawai
   */
   let jumlahPria = null;
+for (const pria of dataYangAkanDilooping) {
+  pria.jenisKelamin === 'M' && jumlahPria++
+}
+  // jumlahPria = 
+  
 
   /*
     TODO 3: Buatlah sebuah variabel bernama "jumlahWanita"
       yang berisi jumlah wanita dari masing masing pegawai
   */
   let jumlahWanita = null;
-
+  for (const wanita of dataYangAkanDilooping) {
+    wanita.jenisKelamin === 'F' && jumlahWanita++
+  }
   /*
     TODO 4: Buatlah sebuah variabel bernama "komentar"
       yang akan mengomentari apakah lebih banyak Pria atau Wanita
@@ -49,6 +61,13 @@ function lakukanLooping(arrPegawai) {
         "Jumlah Pria dan Wanita berimbang"
   */
   let komentar = null;
+  if (jumlahPria > jumlahWanita) {
+    komentar = "Jumlah Pria lebih banyak dari Wanita"
+  }else if(jumlahWanita < jumlahPria){
+    komentar = "Jumlah Wanita lebih banyak dari Pria"
+  }else if(jumlahPria == jumlahWanita){
+    komentar = "Jumlah Pria dan Wanita berimbang"
+  }
 
   // ! JANGAN DIMODIFIKASI
   return {
